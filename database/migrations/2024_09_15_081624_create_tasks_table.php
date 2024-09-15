03791a342a9b5e7ca0ed995f57f3ad58d82e450f
bima-taruna <bimataruna21@gmail.com>
2024-09-15 15:32:28 +0700

    menambahkan beberapa properti ke tasks migration

--- database/migrations/2024_09_15_081624_create_tasks_table.php
+++ database/migrations/2024_09_15_081624_create_tasks_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('judul');
+            $table->string('deskripsi');
+            $table->boolean('selesai');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
