bf444856cf7c072f7c5778fcfdc3e348d6adac34
bima-taruna <bimataruna21@gmail.com>
2024-09-15 18:18:14 +0700

    membuat route untuk register

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * route "/register"
+ * @method "POST"
+ */
+
+Route::post('/register', App\Http\Controller\Api\RegisterController::class)->name('register');
